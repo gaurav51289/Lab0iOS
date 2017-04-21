c0304871706511ca2814232c0ced06c7778faa59
Gaurav Chodwadia <gauravchodwadia@gmail.com>
2017-04-20 21:55:24 -0700

    Initial Commit

---
+++ Lab0/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lab0
+//
+//  Created by Gaurav Chodwadia on 4/20/17.
+//  Copyright © 2017 Gaurav Chodwadia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
